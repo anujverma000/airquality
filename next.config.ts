aa75108b817195e1c118d781ebb810c2f2637787
Anuj Verma <anujverma000@gmail.com>
2025-02-21 13:20:36 +0400

    fixed next config

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.externals = [...config.externals, { knex: 'knex' }]
-    return config
-  },
   headers: async () => [
     {
-      source: '/api/(.*)',
+      source: "/api/(.*)",
       headers: [
-        { key: 'Access-Control-Allow-Credentials', value: 'true' },
-        { key: 'Access-Control-Allow-Origin', value: '*' },
-        { key: 'Access-Control-Allow-Methods', value: 'GET,POST' },
-      ]
-    }
+        { key: "Access-Control-Allow-Credentials", value: "true" },
+        { key: "Access-Control-Allow-Origin", value: "*" },
+        { key: "Access-Control-Allow-Methods", value: "GET,POST" },
+      ],
+    },
   ],
   output: "standalone",
 };
